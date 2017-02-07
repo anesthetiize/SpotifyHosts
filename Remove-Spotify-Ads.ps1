2ab7012420fd573de565ce754f77ee2641466581
Bryan Hernández <bryanhleal@hotmail.com>
2017-02-07 17:01:50 -0600

    Add files via upload

---
+++ Remove-Spotify-Ads.ps1
@@ -0,0 +1,5 @@
+﻿$url = "https://raw.githubusercontent.com/anesthetiize/SpotifyHosts/master/hosts"
+curl $url -OutFile C:\hosts.txt
+$h = gc C:\hosts.txt
+$h > C:\Windows\System32\Drivers\etc\hosts
+cat C:\Windows\System32\Drivers\etc\hosts
