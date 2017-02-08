eed5eecf96544222ad0ffb17ca36e52fd7785b72
Bryan Hernández <bryanhleal@hotmail.com>
2017-02-07 21:58:07 -0600

    Fully Working Script.

--- Remove-Spotify-Ads.ps1
+++ Remove-Spotify-Ads.ps1
@@ -1,5 +1,6 @@
-﻿$url = "https://raw.githubusercontent.com/anesthetiize/SpotifyHosts/master/hosts"
+$url = "https://raw.githubusercontent.com/anesthetiize/SpotifyHosts/master/hosts"
 curl $url -OutFile C:\hosts.txt
 $h = gc C:\hosts.txt
 $h > C:\Windows\System32\Drivers\etc\hosts
-cat C:\Windows\System32\Drivers\etc\hosts
+cd $env:USERPROFILE\AppData\Roaming\Spotify\Apps
+Rename-Item ad.spa adX.spa
