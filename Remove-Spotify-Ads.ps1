ffa75b64e27a6990cb2e97f83146cccf493e06bb
Bryan Hernández <bryanhleal@hotmail.com>
2019-03-27 00:37:35 -0600

    Update Remove-Spotify-Ads.ps1

--- Remove-Spotify-Ads.ps1
+++ Remove-Spotify-Ads.ps1
@@ -6,8 +6,7 @@ $myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myW
 $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
 
 # Check to see if currently running as an administrator
-if ($myWindowsPrincipal.IsInRole($adminRole))
-{
+if ($myWindowsPrincipal.IsInRole($adminRole)){
     # Running as an administrator, so change the title and background colour to indicate this
     $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)";
     $Host.UI.RawUI.BackgroundColor = "DarkBlue";
@@ -15,19 +14,10 @@ if ($myWindowsPrincipal.IsInRole($adminRole))
 }
 else {
     # Not Running as Admin.
-    # Create a new process object that starts PowerShell
     $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-    # Specify the current script path and name as a parameter with added scope and support for scripts with spaces in it's path
     $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
-
-    # Indicate that the process should be elevated
     $newProcess.Verb = "runas";
-
-    # Start the new process
     [System.Diagnostics.Process]::Start($newProcess);
-
-    # Exit from the current, unelevated, process
     Exit;
 }
 
